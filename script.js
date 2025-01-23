605607eac7f1cbecf1071f3afff0796cb32ef815
Sachin Singh <sachinsingh9272@gmail.com>
2025-01-24 00:42:56 +0530

    Added Search Functionality to the Main page

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function toggleMenu() {
     const navbarLinks = document.querySelector('.navbar-links');
     navbarLinks.classList.toggle('active');
 }
+function filterGames() {
+    const searchInput = document.getElementById("searchInput").value.toLowerCase();
+    const gameCards = document.querySelectorAll(".game-card");
+
+    gameCards.forEach((card) => {
+      const gameTitle = card.querySelector("h2").textContent.toLowerCase();
+      if (gameTitle.includes(searchInput)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  }
